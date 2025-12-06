40c8c4a762d080f9e3ceeb52927ae71c180ba51d
Everson Sousa <67715053+evssousa@users.noreply.github.com>
2025-12-06 03:36:59 +0000

    projeto concluído

--- README.md
+++ README.md
@@ -3,10 +3,15 @@ Jogo de adivinhação utilizando conhecimentos básicos de Ruby
 
 Fluxo Principal (Ponto de Partida)
 
-- O jogo deve seguir o seguinte fluxo:
+O jogo deve seguir o seguinte fluxo:
+
 - O sistema sorteia um número aleatório (entre 1 e 100).
 - O sistema inicializa um contador de tentativas em zero.
 - O sistema entra em um loop (while).
-- Dentro do loop: a. O contador de tentativas é incrementado. b. O sistema solicita o palpite ao usuário. c. O palpite é comparado com o número secreto. d. O feedback é exibido.
+- Dentro do loop: 
+    - a. O contador de tentativas é incrementado. 
+    - b. O sistema solicita o palpite ao usuário. 
+    - c. O palpite é comparado com o número secreto. 
+    - d. O feedback é exibido.
 - O loop termina quando o palpite for igual ao número secreto.
 - O sistema exibe a mensagem de vitória, incluindo o número total de tentativas.
--- main.rb
+++ main.rb
@@ -1,5 +1,43 @@
-def start()
-    print "Digite um número: "
+def limpar_tela
+  if RUBY_PLATFORM =~ /win32|win64|\.net/
+    system 'cls'
+  else
+    system 'clear'
+  end
+end
+
+def msg_inicial
+    puts "=== SEJA BEM-VINDO AO JOGO DE ADIVINHAÇÃO! ==="
+    puts "======= Acerte o número e se divirta! ========"
+    puts "=============================================="
+end
+
+def start
+    msg_inicial()
+    numero_secreto = rand(1..100)
+    contador = 0
+    numero = 0
+
+    while numero != numero_secreto
+        print "Digite um número entre 1-100: "
+        numero = gets.chomp.to_i
+        puts  "=" * 46
+        
+        if numero == numero_secreto
+            limpar_tela()
+            msg_inicial()
+            puts "Você adivinhou o número, parabéns!"
+            puts "Quantidade de tentativas: #{contador}"
+        else
+            limpar_tela()
+            msg_inicial()
+            contador += 1
+            puts "Você errou o número, tente novamente."
+            puts "Tentativas: #{contador}"
+            puts  "=" * 46
+        end
+    end
+
 end
 
 start()
